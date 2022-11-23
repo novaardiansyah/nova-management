ee4b22c95c4f8fb66d756fbe82f97dd3c9561327
Nova  Ardiansyah <novaardiansyah78@gmail.com>
2022-11-23 21:19:25 +0700

    Set up the user authorization feature

--- application/modules/auth/views/layout/header.php
+++ application/modules/auth/views/layout/header.php
@@ -3,16 +3,14 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nova Management</title>
-    
+    <title>Authorization - Nova Management</title>
     <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/css/main/app.css'); ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/css/main/app-dark.css'); ?>">
-    <link rel="shortcut icon" href="<?= base_url('assets/images/favicon.ico'); ?>" type="image/png">
-    
+    <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/css/pages/auth.css'); ?>">
+    <link rel="shortcut icon" href="<?= base_url('assets/images/favicon.ico'); ?>" type="image/png" />
+
     <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/css/shared/iconly.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/extensions/toastify-js/src/toastify.css'); ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/mazer/assets/extensions/sweetalert2/sweetalert2.min.css'); ?>">
-
+    
     <?php if (isset($style)) : ?>
       <?php foreach ($style as $key => $value) : ?>
         <link rel="stylesheet" href="<?= $value ?>" />
@@ -20,3 +18,4 @@
     <?php endif; ?>
   </head>
   <body>
+    <div id="auth">
